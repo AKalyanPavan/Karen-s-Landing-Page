66edf42fff451e5073df6e729d2de95022513c48
AKalyanPavan <pavanaravelli690@gmail.com>
2024-08-29 21:20:10 +0530

    Removing Icon classes

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -175,7 +175,7 @@ function Homepage() {
 					<div className="text-[32px] font-bold  my-[50px]">Our Services</div>
 					<div className="grid min-[1100px]:grid-cols-3 min-[700px]:grid-cols-2 grid-cols-1 gap-[20px]">
 						<div className="p-[20px] text-center rounded-[10px]">
-							<svg className="mx-auto" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#377DFF" class="bi bi-clipboard2-check-fill" viewBox="0 0 16 16">
+							<svg className="mx-auto" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#377DFF" viewBox="0 0 16 16">
 								<path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5"/>
 								<path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585q.084.236.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5q.001-.264.085-.5m6.769 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708"/>
 							</svg>
@@ -183,7 +183,7 @@ function Homepage() {
 							<div className="text-[#677788]">Creates certified true copies of documents like birth certificates and diplomas, ensuring they are accurate and match the originals.</div>
 						</div>
 						<div className="p-[20px] rounded-[10px]">
-							<svg className='mx-auto' xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#377DFF" class="bi bi-file-earmark-pdf-fill" viewBox="0 0 16 16">
+							<svg className='mx-auto' xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#377DFF" viewBox="0 0 16 16">
 								<path d="M5.523 12.424q.21-.124.459-.238a8 8 0 0 1-.45.606c-.28.337-.498.516-.635.572l-.035.012a.3.3 0 0 1-.026-.044c-.056-.11-.054-.216.04-.36.106-.165.319-.354.647-.548m2.455-1.647q-.178.037-.356.078a21 21 0 0 0 .5-1.05 12 12 0 0 0 .51.858q-.326.048-.654.114m2.525.939a4 4 0 0 1-.435-.41q.344.007.612.054c.317.057.466.147.518.209a.1.1 0 0 1 .026.064.44.44 0 0 1-.06.2.3.3 0 0 1-.094.124.1.1 0 0 1-.069.015c-.09-.003-.258-.066-.498-.256M8.278 6.97c-.04.244-.108.524-.2.829a5 5 0 0 1-.089-.346c-.076-.353-.087-.63-.046-.822.038-.177.11-.248.196-.283a.5.5 0 0 1 .145-.04c.013.03.028.092.032.198q.008.183-.038.465z"/>
 								<path fill-rule="evenodd" d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2m5.5 1.5v2a1 1 0 0 0 1 1h2zM4.165 13.668c.09.18.23.343.438.419.207.075.412.04.58-.03.318-.13.635-.436.926-.786.333-.401.683-.927 1.021-1.51a11.7 11.7 0 0 1 1.997-.406c.3.383.61.713.91.95.28.22.603.403.934.417a.86.86 0 0 0 .51-.138c.155-.101.27-.247.354-.416.09-.181.145-.37.138-.563a.84.84 0 0 0-.2-.518c-.226-.27-.596-.4-.96-.465a5.8 5.8 0 0 0-1.335-.05 11 11 0 0 1-.98-1.686c.25-.66.437-1.284.52-1.794.036-.218.055-.426.048-.614a1.24 1.24 0 0 0-.127-.538.7.7 0 0 0-.477-.365c-.202-.043-.41 0-.601.077-.377.15-.576.47-.651.823-.073.34-.04.736.046 1.136.088.406.238.848.43 1.295a20 20 0 0 1-1.062 2.227 7.7 7.7 0 0 0-1.482.645c-.37.22-.699.48-.897.787-.21.326-.275.714-.08 1.103"/>
 							</svg>
@@ -200,7 +200,7 @@ function Homepage() {
 							<div className="text-[#677788]">Assists with creating and notarizing legal wills and power of attorney documents, ensuring compliance with all legal requirements.</div>
 						</div>
 						<div className="p-[20px] rounded-[10px]">
-							<svg className="mx-auto" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#377DFF" class="bi bi-ui-checks" viewBox="0 0 16 16">
+							<svg className="mx-auto" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#377DFF" viewBox="0 0 16 16">
 								<path d="M7 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5zM2 1a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm0 8a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-2a2 2 0 0 0-2-2zm.854-3.646a.5.5 0 0 1-.708 0l-1-1a.5.5 0 1 1 .708-.708l.646.647 1.646-1.647a.5.5 0 1 1 .708.708zm0 8a.5.5 0 0 1-.708 0l-1-1a.5.5 0 0 1 .708-.708l.646.647 1.646-1.647a.5.5 0 0 1 .708.708zM7 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5zm0-5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 8a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5"/>
 							</svg>
 							<div className="text-[20px] text-[#1E2022] font-medium my-[30px]">Ontario Court Forms</div>
